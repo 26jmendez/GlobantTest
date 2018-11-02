61a673b0b282ced8b1fc0412f335b724795e2a38
Javier Mendez Garcia <javier.mendez@globant.com>
2018-11-02 17:40:18 -0500

    Inclusion de estilos

---
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+import {
+    SUN,
+} from './../../../constants/weathers';
+import './styles.css';
+
+const WeatherData = () => (
+    <div className="weatherDataCont">
+        <WeatherTemperature temperature={20} weatherState={SUN} />
+        <WeatherExtraInfo humidity={90} wind={'15 m/s'} />
+    </div>
+);
+
+export default WeatherData;
